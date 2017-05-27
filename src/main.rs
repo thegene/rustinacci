79154e78dd443c951b8e1524e3df0dfbf7471c01
Eugene Westbrook <eugenewestbrook@gmail.com>
2017-05-27 13:05:05 -0500

    I think I'm doing borrowing corectly here

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 trait HasValue {
     fn get_value(&self) -> i32;
+    fn get_previous_value(&self) -> i32;
 }
 
 struct Null;
@@ -8,25 +9,38 @@ impl HasValue for Null {
     fn get_value(&self) -> i32 {
         0
     }
+
+    fn get_previous_value(&self) -> i32 {
+        0
+    }
 }
 
 struct Node<T> {
     value: i32,
-    pointer: T
+    previous: T
 }
 
 impl<T: HasValue> Node<T> {
     fn new(previous: T) -> Node<T> {
         Node {
-            value: previous.get_value() + 1,
-            pointer: previous
+            value: Node::find_new_value(&previous),
+            previous: previous
+        }
+    }
+
+    fn find_new_value(previous: &T) -> i32 {
+        let previous_value = previous.get_value();
+        if previous_value == 0 {
+            1
+        } else {
+            previous_value + previous.get_previous_value()
         }
     }
 }
 
 fn find_fib(fib_number: i32) {
-    let null: Null = Null{};
-    let node = Node::new(null);
+    let head: Null = Null{};
+    let node = Node::new(head);
 
     println!("{}", node.value);
 }
