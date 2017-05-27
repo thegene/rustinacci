03f002b16b6b6f008b85fa042da25996ed7dda5a
Eugene Westbrook <eugenewestbrook@gmail.com>
2017-05-27 11:54:30 -0500

    Have a node with a Null pointer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
+trait HasValue {
+    fn get_value(&self) -> i32;
+}
+
+struct Null;
+
+struct Node {
+    value: i32,
+    pointer: Null
+}
+
+impl Node {
+    fn new(previous: Null) -> Node {
+        Node {
+            value: 0,
+            pointer: previous
+        }
+    }
+}
+
 fn find_fib(fib_number: i32) {
-    println!("{}", fib_number);
+    let null: Null = Null{};
+    let node: Node = Node::new(null);
+
+    println!("{}", node.value);
 }
 
 fn main() {
